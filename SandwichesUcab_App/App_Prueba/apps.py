6ff3bdbf99404738b48aae889e9573061116f52c
hipsteric0 <aleromerosurf@gmail.com>
2021-01-29 23:33:14 -0400

    estructura general y pagina home

--- SandwichesUcab_App/App_Prueba/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppPruebaConfig(AppConfig):
-    name = 'App_Prueba'
--- SandwichesUcab_App/SandwichesUcab_App/settings.py
+++ SandwichesUcab_App/SandwichesUcab_App/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os.path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = '47-7gx9^829bn6yn7t_nzm*muq$*quy8njn8xjz&lp54f9fuqe'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -37,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'App_Prueba',
+    'Home',
+
 ]
 
 MIDDLEWARE = [
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'SandwichesUcab_App.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
