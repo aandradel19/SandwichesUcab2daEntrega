eff572e03d891bde4322a47d169db6c6536d4454
hipsteric0 <aleromerosurf@gmail.com>
2021-01-31 19:49:10 -0400

    Arreglo de urls para una sola app

--- SandwichesUcab_App/Home/urls.py
+++ SandwichesUcab_App/Home/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.homePage, name='homePage'),
     
+
+    path('',views.homePage, name='homePage'),
+    path('pedidos/',views.pedidosPage, name='pedidosPage'),
+    path('totalizacion/',views.totalizacionPage, name='totalizacionPage'),
+    path('historial/',views.historialPage, name='historialPage'),
+    path('graciasVuelvaPronto/',views.graciasVuelvaProntoPage, name='graciasVuelvaProntoPage'),
 ]
--- SandwichesUcab_App/Home/views.py
+++ SandwichesUcab_App/Home/views.py
@@ -5,3 +5,15 @@ from django.http import HttpResponse
 
 def homePage(request):
     return render(request, 'home.html')
+
+def pedidosPage(request):
+    return render(request, 'pedidos.html')
+
+def totalizacionPage(request):
+    return render(request, 'totalizacion.html')
+
+def historialPage(request):
+    return render(request, 'historial.html')
+
+def graciasVuelvaProntoPage(request):
+    return render(request, 'graciasVuelvaPronto.html')
