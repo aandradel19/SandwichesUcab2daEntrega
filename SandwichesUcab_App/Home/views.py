87f25c6a0e3ed2e937ecab5e21426505565600bd
aandradel19 <aandradel.17@est.ucab.edu.ve>
2021-02-02 11:04:26 -0400

    Reportes completos sin parámetros

--- SandwichesUcab_App/Home/views.py
+++ SandwichesUcab_App/Home/views.py
@@ -104,16 +104,93 @@ def graciasVuelvaProntoPage(request):
     return render(request, 'graciasVuelvaPronto.html')
     
 def ventasPorClientesPage(request):
-    return render(request, 'ventasPorClientes.html')
+    cursor = connection.cursor()
+    cursor.execute('''SELECT  ped.id Factura, ped.fecha_pedido Fecha, ped.cedula Cliente, 
+(select count(*) 
+    from Home_sandwich san 
+        where san.pedido_id_id = ped.id) Cantidad_sandwiches,
+(select sum(ingredientes.monto + tamanos.monto) || " Bs."
+    from (select COALESCE (sum(ing.costo_ingrediente),0) as monto
+             from Home_ingrediente ing, Home_sandwich san, Home_sandwich_ingrediente si
+                 where ing.id = si.ingrediente_id_id and si.sandwich_id_id = san.id and san.pedido_id_id = ped.id)as ingredientes, 
+         (select sum(tam.costo_tamano) as monto
+             from Home_tamano tam, Home_sandwich san
+                 where san.tamano_id_id = tam.id and san.pedido_id_id = ped.id) as tamanos) as Monto_sandwiches
+from Home_pedido ped
+where ped.cedula = "223344"
+order by 5 desc ''')
+    salida = dictfetchall(cursor)
+    return render(request, 'ventasPorClientes.html', {
+        'venta': salida
+    })
 
 def ventasPorDiaPage(request):
-    return render(request, 'ventasPorDia.html')
+    cursor = connection.cursor()
+    cursor.execute('''SELECT  ped.id Factura, ped.fecha_pedido Fecha, ped.cedula Cliente, 
+(select count(*) 
+    from Home_sandwich san 
+        where san.pedido_id_id = ped.id) Cantidad_sandwiches,
+(select sum(ingredientes.monto + tamanos.monto) || " Bs."
+    from (select COALESCE (sum(ing.costo_ingrediente),0) as monto
+             from Home_ingrediente ing, Home_sandwich san, Home_sandwich_ingrediente si
+                 where ing.id = si.ingrediente_id_id and si.sandwich_id_id = san.id and san.pedido_id_id = ped.id)as ingredientes, 
+         (select sum(tam.costo_tamano) as monto
+             from Home_tamano tam, Home_sandwich san
+                 where san.tamano_id_id = tam.id and san.pedido_id_id = ped.id) as tamanos) Monto_sandwiches
+from Home_pedido ped
+where ped.fecha_pedido = "2021-02-02" ''')
+    salida = dictfetchall(cursor)
+    return render(request, 'ventasPorDia.html', {
+        'venta': salida
+    })
 
 def ventasPorIngredientesPage(request):
-    return render(request, 'ventasPorIngredientes.html')
+    cursor = connection.cursor()
+    cursor.execute('''SELECT  ped.id Factura, ped.fecha_pedido Fecha, ped.cedula Cliente, 
+(select count(*) 
+    from Home_sandwich san 
+        where san.pedido_id_id = ped.id and san.id in (select sand2.id 
+                                                        from Home_sandwich sand2, Home_ingrediente ing, Home_sandwich_ingrediente si  
+                                                            where si.sandwich_id_id = sand2.id and si.ingrediente_id_id = "2")) Cantidad_sandwiches,
+(select sum(ingredientes.monto + tamanos.monto) || " Bs."
+    from (select COALESCE (sum(ing.costo_ingrediente),0) as monto
+             from Home_ingrediente ing, Home_sandwich san, Home_sandwich_ingrediente si
+                 where ing.id = si.ingrediente_id_id and si.sandwich_id_id = san.id and san.pedido_id_id = ped.id and san.id in (select sand2.id 
+                                                        from Home_sandwich sand2, Home_ingrediente ing, Home_sandwich_ingrediente si  
+                                                            where si.sandwich_id_id = sand2.id and si.ingrediente_id_id = "2"))as ingredientes, 
+         (select sum(tam.costo_tamano) as monto
+             from Home_tamano tam, Home_sandwich san
+                 where san.tamano_id_id = tam.id and san.pedido_id_id = ped.id and san.id in (select sand2.id 
+                                                        from Home_sandwich sand2, Home_ingrediente ing, Home_sandwich_ingrediente si  
+                                                            where si.sandwich_id_id = sand2.id and si.ingrediente_id_id = "2")) as tamanos) Monto_sandwiches
+from Home_pedido ped 
+where ped.id in (select pp.id from Home_pedido pp, Home_sandwich ss where ss.pedido_id_id = pp.id and ss.id in (select sand2.id 
+                                                        from Home_sandwich sand2, Home_ingrediente ing, Home_sandwich_ingrediente si  
+                                                            where si.sandwich_id_id = sand2.id and si.ingrediente_id_id = "2"))''')
+    salida = dictfetchall(cursor)
+    return render(request, 'ventasPorIngredientes.html', {
+        'venta': salida
+    })
 
 def ventasPorTamanoPage(request):
-    return render(request, 'ventasPorTamano.html')
+    cursor = connection.cursor()
+    cursor.execute('''SELECT ped.id Factura, ped.fecha_pedido Fecha, ped.cedula Cliente, 
+(select count(*) 
+    from Home_sandwich san 
+        where san.pedido_id_id = ped.id and san.tamano_id_id = "2") Cantidad_sandwiches,
+(select sum(ingredientes.monto + tamanos.monto) || " Bs."
+    from (select COALESCE (sum(ing.costo_ingrediente),0) as monto
+             from Home_ingrediente ing, Home_sandwich san, Home_sandwich_ingrediente si
+                 where ing.id = si.ingrediente_id_id and si.sandwich_id_id = san.id and san.pedido_id_id = ped.id and san.tamano_id_id = "2")as ingredientes, 
+         (select sum(tam.costo_tamano) as monto
+             from Home_tamano tam, Home_sandwich san
+                 where san.tamano_id_id = tam.id and san.pedido_id_id = ped.id and san.tamano_id_id = "2") as tamanos) Monto_sandwiches
+from Home_pedido ped 
+where ped.id in (select pp.id from Home_pedido pp, Home_sandwich ss where ss.pedido_id_id = pp.id and ss.tamano_id_id = "2")''')
+    salida = dictfetchall(cursor)
+    return render(request, 'ventasPorTamano.html', {
+        'venta': salida
+    })
 
 def ventasTotalesPage(request):
     cursor = connection.cursor()
@@ -130,7 +207,6 @@ def ventasTotalesPage(request):
                                 where san.tamano_id_id = tam.id and san.pedido_id_id = ped.id) as tamanos) Monto_sandwiches
                 from Home_pedido ped''')
     salida = dictfetchall(cursor)
-    print(cursor)
     return render(request, 'ventasTotales.html', {
         'venta': salida
     })
